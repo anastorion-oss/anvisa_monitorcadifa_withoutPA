698601eea62813715da407f3cdf97d298eb9be3e
anastorion-oss <anastorion@gmail.com>
2026-08-24 17:45:00 -0300

    Update email sending to multiple recipients

--- enviar_email.py
+++ enviar_email.py
@@ -5,7 +5,10 @@ from email.mime.text import MIMEText
 gmail_user = os.environ["GMAIL_USER"]
 gmail_password = os.environ["GMAIL_PASSWORD"]
 
-destinatario = "ana.storion@ache.com.br"
+destinatarios = [
+    "ana.storion@ache.com.br".
+    "lais.horta@ache.com.br"
+]
 
 with open("resultado.txt", "r", encoding="utf-8") as f:
     conteudo = f.read()
@@ -14,10 +17,15 @@ mensagem = MIMEText(conteudo)
 
 mensagem["Subject"] = "MONITOR CADIFA"
 mensagem["From"] = gmail_user
-mensagem["To"] = destinatario
+mensagem["To"] = ", ".join(destinatarios)
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as servidor:
     servidor.login(gmail_user, gmail_password)
-    servidor.send_message(mensagem)
+    servidor.sendmail(
+        gmail_user,
+        destinatarios,
+        mensagem.as_string()
+    )
+    
 
 print("E-mail enviado com sucesso.")
