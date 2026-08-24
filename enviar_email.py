fd4f7fe4a7332bf48424dc161457b3dbfb83ffe0
anastorion-oss <anastorion@gmail.com>
2026-08-24 16:12:01 -0300

    Add email sending functionality using Gmail SMTP

---
+++ enviar_email.py
@@ -0,0 +1,22 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+gmail_user = os.environ["GMAIL_USER"]
+gmail_password = os.environ["GMAIL_PASSWORD"]
+
+destinatario = "ana.storion@ache.com.br"
+
+mensagem = MIMEText(
+    "Este é um teste de envio de e-mail pelo GitHub Actions."
+)
+
+mensagem["Subject"] = "Teste CADIFA"
+mensagem["From"] = gmail_user
+mensagem["To"] = destinatario
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as servidor:
+    servidor.login(gmail_user, gmail_password)
+    servidor.send_message(mensagem)
+
+print("E-mail enviado com sucesso.")
